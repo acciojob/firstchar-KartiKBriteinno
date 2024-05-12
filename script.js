a9851d43578c26df7e920d8d881693715e9b8366
KartiKBriteinno <153173760+KartiKBriteinno@users.noreply.github.com>
2024-05-12 14:41:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function firstChar(text) {
-  // your code here
+  // Iterate over each character in the input string
+  for (let i = 0; i < text.length; i++) {
+    // Check if the current character is not a space
+    if (text[i] !== ' ') {
+      // Return the first non-space character
+      return text[i];
+    }
+  }
+  // If the input string only contains spaces or is empty, return an empty string
+  return '';
 }
+
+console.log(firstChar(' Rosa Parks ')); // Output: 'R'
+console.log(firstChar(' Hello World ')); // Output: 'H'
+console.log(firstChar(' ')); // Output: ''
+console.log(firstChar('')); // Output: ''
 
 // Do not change the code below
 
